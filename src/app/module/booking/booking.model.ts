f250851be6dced1a1421569b1fa9bb5821c10bdc
reZerOR <gkraihan14k@gmail.com>
2024-07-16 22:24:20 +0600

    booking model created

---
+++ src/app/module/booking/booking.model.ts
@@ -0,0 +1,39 @@
+import { model, Schema } from "mongoose";
+import { TBooking } from "./booking.interface";
+
+const bookingSchema = new Schema<TBooking>({
+  user: {
+    type: Schema.Types.ObjectId,
+    required: [true, "User id required"],
+    ref: "User",
+  },
+  facility: {
+    type: Schema.Types.ObjectId,
+    required: [true, "Facility id required"],
+    ref: "Facility",
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  startTime: {
+    type: String,
+    required: true,
+  },
+  endTime: {
+    type: String,
+    required: true,
+  },
+  payableAmount: {
+    type: Number,
+    required: true,
+  },
+  isBooked: {
+    type: String,
+    enum: ["confirmed", "canceled"],
+    default: "confirmed",
+  },
+});
+
+
+export const Booking = model<TBooking>('Booking', bookingSchema)
